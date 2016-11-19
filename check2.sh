26e691eeac49a61285b6b25a5e8497cfcfb27a48
Luca Chiodini <luca@chiodini.org>
2016-11-19 14:55:05 +0100

    Tmp

--- check2.sh
+++ check2.sh
@@ -1,7 +1,8 @@
-echo '<testsuite tests="3">'
-echo '    <testcase classname="foo1" name="ASuccessfulTest"/>'
-echo '	    <testcase classname="foo3" name="AFailingTest">'
-echo '<failure type="NotEnoughFoo"> details about failure </failure>'
-echo '	        </testcase>'
-echo '	</testsuite>'
+echo '' > report.xml
+echo '<testsuite tests="3">' >> report.xml
+echo '    <testcase classname="foo1" name="ASuccessfulTest"/>' >> report.xml
+echo '	    <testcase classname="foo3" name="AFailingTest">' >> report.xml
+echo '<failure type="NotEnoughFoo"> details about failure </failure>' >> report.xml
+echo '	        </testcase>' >> report.xml
+echo '	</testsuite>' >> report.xml
 
