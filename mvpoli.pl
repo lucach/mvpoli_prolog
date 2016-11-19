5fb38134107db668a967f326610ed7f36aca37a9
Luca Chiodini <luca@chiodini.org>
2016-11-19 15:48:05 +0100

    is_monomial, is_varpower, is_polynomial

--- mvpoli.pl
+++ mvpoli.pl
@@ -1,1 +1,13 @@
-foo.
+is_monomial(m(_C, TD, VPs)) :-
+    integer(TD),
+    TD >= 0,
+    is_list(VPs).
+
+is_varpower(v(Power, VarSymbol)) :-
+    integer(Power),
+    Power >= 0,
+    atom(VarSymbol).
+
+is_polynomial(poly(Monomials)) :-
+    is_list(Monomials),
+    foreach(member(M, Monomials), is_monomial(M)).
--- tests.pl
+++ tests.pl
@@ -1,2 +1,1 @@
-foo = foo.
-foo = bar.
+m(_, _, [v(2, x), v(2, y)] = m(_, _, VPs), foreach(member(VP, VPs), is_varpower(VP)).
