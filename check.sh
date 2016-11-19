80cbb92b1e40661702567475a48e67a29b5b0589
Luca Chiodini <luca@chiodini.org>
2016-11-19 14:51:16 +0100

    Tmp

--- check.sh
+++ check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-res=`echo $1 | swipl -q -s $2`
-if [ "$res" == "true." ]; then
-	exit 0
-else
-	exit 1
-fi
+while read test
+do
+	echo "$test"
+        res=`echo $test | swipl -q -s mvpoli.pl`
+	if [ "$res" == "true." ]; then
+		exit 0
+	else
+		exit 1
+	fi
+done < tests.pl
---
+++ check2.sh
@@ -0,0 +1,7 @@
+echo '<testsuite tests="3">'
+echo '    <testcase classname="foo1" name="ASuccessfulTest"/>'
+echo '	    <testcase classname="foo3" name="AFailingTest">'
+echo '<failure type="NotEnoughFoo"> details about failure </failure>'
+echo '	        </testcase>'
+echo '	</testsuite>'
+
