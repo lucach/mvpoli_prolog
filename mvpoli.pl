9fb197fa0b88edf51399734ab105891a7988b064
Luca Chiodini <luca@chiodini.org>
2016-11-19 16:40:09 +0100

    get_power and get_totaldegree

--- mvpoli.pl
+++ mvpoli.pl
@@ -11,3 +11,13 @@ is_varpower(v(Power, VarSymbol)) :-
 is_polynomial(poly(Monomials)) :-
     is_list(Monomials),
     foreach(member(M, Monomials), is_monomial(M)).
+
+get_power(v(Power, VarSymbol), Power) :-
+    is_varpower(v(Power, VarSymbol)).
+
+get_totaldegree(m(_, 0, [])) :- !.
+
+get_totaldegree(m(_C, TD, [VP | VPs])) :-
+    get_totaldegree(m(_, PartialDegree, VPs)),
+    get_power(VP, FirstElementDegree),
+    TD is FirstElementDegree + PartialDegree.
--- tests.pl
+++ tests.pl
@@ -1,2 +1,5 @@
 is_varpower(v(3, a)).
 m(_, _, [v(2, x), v(2, y)]) = m(_, _, VPs), foreach(member(VP, VPs), is_varpower(VP)).
+get_power(v(2, x), P), P = 2.
+get_totaldegree(m(_, TD, [])), TD = 0.
+get_totaldegree(m(_, TD, [v(3, t), v(1, w), v(1, y)])), TD = 5.
