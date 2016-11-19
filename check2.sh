641d964efb6f0865f4b25ffced95fcc6d9a56c30
Luca Chiodini <luca@chiodini.org>
2016-11-19 15:07:49 +0100

    Tmp

--- check2.sh
+++
@@ -1,8 +0,0 @@
-echo '' > report.xml
-echo '<testsuite tests="3">' >> report.xml
-echo '    <testcase classname="foo1" name="ASuccessfulTest"/>' >> report.xml
-echo '	    <testcase classname="foo3" name="AFailingTest">' >> report.xml
-echo '<failure type="NotEnoughFoo"> details about failure </failure>' >> report.xml
-echo '	        </testcase>' >> report.xml
-echo '	</testsuite>' >> report.xml
-
---
+++ run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+lines=`cat tests.pl | wc -l`
+echo '<testsuite tests="'$lines'">' > report.xml
+i=1
+while read test
+do
+	res=`echo $test | swipl -q -s mvpoli.pl`
+	if [ $res = "true." ]; then
+                echo '<testcase classname="test'$i'" name="test'$i'"/>' >> report.xml
+        else
+                echo '<testcase classname="test'$i'" name="test'$i'">' >> report.xml
+		echo '<failure type="Fail"> '$res' </failure>' >> report.xml
+		echo '</testcase>' >> report.xml
+        fi
+done < tests.pl
+echo '</testsuite>' >> report.xml
+
