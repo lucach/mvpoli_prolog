lucach/mvpoli_prolog

commit be0416df312221264a80e8b87d43489df1cffc9c

Fix sorting of monomials with same varpower (same var and same exp)

mvpoli.pl
@@ -137,17 +137,21 @@ parse_monomial(E1 * E2, m(C, TD, VPs)) :-
     append([VP], OtherVPs, VPs).
 
 %%      exponentCompareVP(Operator, v(Exp1, _Var1), v(Exp2, _Var2))
-%       True if Operator is '<' and Exp1 is less than (or equal to) Exp2
-%         or if Operator is '>' and Exp1 is greater than Exp2.
+%       True if Operator is '<' and Exp1 is less than Exp2
+%         or if Operator is '>' and Exp1 is greater than Exp2
+%         or if Operator is '=' and Exp1 is equal to Exp2.
 
 exponentCompareVP(<, v(Exp1, _Var1), v(Exp2, _Var2)) :-
-    Exp1 =< Exp2,
+    Exp1 < Exp2,
     !.
 
 exponentCompareVP(>, v(Exp1, _Var1), v(Exp2, _Var2)) :-
     Exp1 > Exp2,
     !.
 
+exponentCompareVP(>, v(Exp1, _Var1), v(Exp2, _Var2)) :-
+    Exp1 = Exp2,
+    !.
 
 %%      lexicographicallyCompareVP(Operator, v(_P1, Var1), v(_P2, Var2))
 %       True if Operator is '<' and Var1 comes before Var2 in a lex. order

tests.pl
@@ -33,6 +33,7 @@ as_polynomial(-1 * x + x * y, poly([m(-1, 1, [v(1, x)]), m(1, 2, [v(1, x), v(1, 
 as_polynomial(x * y^2 + x^2 * y, poly([m(1, 3, [v(1, x), v(2, y)]), m(1, 3, [v(2, x), v(1, y)])])).
 as_polynomial(a*c+a^2+a*b+a, poly([m(1, 1, [v(1, a)]), m(1, 2, [v(1, a), v(1, b)]), m(1, 2, [v(1, a), v(1, c)]), m(1, 2, [v(2, a)])])).
 as_polynomial(0*a+x, poly([m(1, 1, [v(1, x)])])).
+as_polynomial(a^2 + a*c + a + a*z, poly([m(1, 1, [v(1, a)]), m(1, 2, [v(1, a), v(1, c)]), m(1, 2, [v(1, a), v(1, z)]), m(1, 2, [v(2, a)])])).
 
 %%%% polyval
 

commit 62996d292a4ffdcdf68f61a59ec714cb7cee7323

Fix stupid copy-paste bug introduced in be0416d

mvpoli.pl
@@ -149,7 +149,7 @@ exponentCompareVP(>, v(Exp1, _Var1), v(Exp2, _Var2)) :-
     Exp1 > Exp2,
     !.
 
-exponentCompareVP(>, v(Exp1, _Var1), v(Exp2, _Var2)) :-
+exponentCompareVP(=, v(Exp1, _Var1), v(Exp2, _Var2)) :-
     Exp1 = Exp2,
     !.
 